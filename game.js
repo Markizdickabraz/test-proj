b7959a701c147034415e27c1337aa81dd8b4ac51
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2024-02-25 21:31:03 +0200

    додано текст при повторному кліку на відкриту коробку і прибрав можливіть клікнути ще раз

--- game.js
+++ game.js
@@ -1,16 +1,15 @@
 let modalIsOpen = false;
-
 const gameItems = document.querySelectorAll('.game__item');
-const gameModal = document.querySelector('.game__modal')
+const gameModal = document.querySelector('.game__modal');
+const gameContainer = document.querySelector('.game__container');
+const gameList = document.querySelector('.game__list');
 
 gameItems.forEach(item => {
     item.addEventListener('click', function () {
         const viewportWidth = window.innerWidth;
-        
         const box = item.children[1];
-        
-        box.style.transition = 'top 0.5s ease';
-        
+        item.style.scale = '1.1';
+
         let newTop;
         if (viewportWidth >= 768 && viewportWidth <= 899) {
             newTop = '-45px';
@@ -26,9 +25,21 @@ gameItems.forEach(item => {
         
         setTimeout(() => {
             if (modalIsOpen) {
+                if (item.classList[1] === 'open') {
+                    const boxIsOpen = document.querySelector('.boxOpen');
+                    if (!boxIsOpen) {
+                    gameContainer.insertAdjacentHTML('beforeend', `<h2 class='boxOpen'>THIS BOX IS OPEN...</h2>`);
+                    const boxIsOpen = document.querySelector('.boxOpen');
+                        setTimeout(() => {
+                            boxIsOpen.remove();
+                        }, 1000);
+                    }
+                     return;
+                    }
                 window.location.href = "./winner.html";
             } else {
                 openModal();
+                item.classList.add('open');
                 modalIsOpen = true;
             }
         }, 600);
